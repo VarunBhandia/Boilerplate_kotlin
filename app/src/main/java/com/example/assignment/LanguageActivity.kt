01eb327aa6d555873c3d58d29453f7f93a77ba30
varunbhandia <varunbhandia@gmail.com>
2020-06-16 00:51:30 +0530

    Added code to show language dynamicallay

--- app/src/main/java/com/example/assignment/LanguageActivity.kt
+++ app/src/main/java/com/example/assignment/LanguageActivity.kt
@@ -2,11 +2,38 @@ package com.example.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class LanguageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language)
+
+        val languages = resources.getStringArray(R.array.Languages)
+        val languages_details = resources.getStringArray(R.array.languages_details)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@LanguageActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + languages_details[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 }
